f8a83402960177a11fd4ec0c9117889a607dab74
David Morley <davidkmorley@gmail.com>
2018-02-21 10:41:23 +0000

    still need to finish this task

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def create
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Article.destroy_all
+
+10.times do
+  article = Article.new(title: Faker::GameOfThrones.character, content: Faker::StarWars.wookiee_sentence )
+  article.save!
+end
